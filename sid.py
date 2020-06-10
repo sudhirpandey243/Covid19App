ad2eeae9ca73270b55d6bfe08da3d40a336c1a63
sudhirpandey243 <66723645+sudhirpandey243@users.noreply.github.com>
2020-06-10 20:32:37 +0530

    Add files via upload

---
+++ sid.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index1N.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 7)
+
+    return render_template('index1N.html', prediction_text='Covid19 Risk  {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
